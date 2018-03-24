bed56eb7098574f8871328b82aa6aa3d8816178e
Antonio Alex Marques Albuquerque <alex.marques@textiluniao.com.br>
2018-03-24 09:15:57 -0300

    first commit

---
+++ noticias.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    var categoria = req.url;
+    
+    if(categoria == '/moda'){
+        res.end("<html><body>Portal de moda</body></html>");    
+    } else if(categoria == '/tecnologia'){
+        res.end("<html><body>Portal de tecnologia</body></html>");    
+    } else {
+        res.end("<html><body>Portal de notícias</body></html>");
+    }
+
+    
+});
+
+server.listen(3000);
+
+
